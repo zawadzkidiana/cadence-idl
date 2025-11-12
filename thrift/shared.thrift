63b7e01d0963afc7ade25fe6122c33853b2af282
Diana Zawadzki <dzawa@live.de>
2025-11-12 15:35:38 -0800

    Add reason to failover domain request
    Signed-off-by: Diana Zawadzki <dzawa@live.de>

--- thrift/shared.thrift
+++ thrift/shared.thrift
@@ -1212,6 +1212,7 @@ struct FailoverDomainRequest {
  // only applicable to active-active domains where
  // specific cluster-attributes are being failed over
  30: optional ActiveClusters activeClusters
+ 40: optional string reason
 }
 
 struct FailoverDomainResponse {
